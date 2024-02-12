f2bc84d38204bdba1d7533cd19dcd927b2fdce58
khalidtouch <khalidhulabaloo9@gmail.com>
2024-02-12 14:55:42 +0100

    implemented the infinity spinner

--- ritten-clone/src/App.jsx
+++ ritten-clone/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Header />
   
         <Routes>
-          <Route path='/blog' element={<NewBlogPage />} />
+          <Route path='/blog' element={<NewBlogPage isLoading='true' />} />
           <Route path='/' element={<HomePage />} />
         </Routes>
       </Router>
--- ritten-clone/src/pages/new/NewBlogPage.jsx
+++ ritten-clone/src/pages/new/NewBlogPage.jsx
@@ -1,9 +1,11 @@
 import { TextField } from '@mui/material';
 import React, { Component } from 'react'
 import './NewBlogPage.css'
+import { InfinitySpin } from 'react-loader-spinner'
 
 class NewBlogPage extends Component {
     render() {
+        const params = this.props
         return (
             <div className='new-blog-page'>
                 <div className='text-field-container'>
@@ -47,7 +49,18 @@ class NewBlogPage extends Component {
                         variant='Contained'
                         size='large'
                         onClick={() => { }}
-                    >Create</button>
+                    >
+                        {
+                            params.isLoading ? (<div className='button-create-blog-content'>
+                                <div className='button-create-blog-text'>Create</div>
+                                <InfinitySpin className='button-spinner' />
+                            </div>) : (<div className='button-create-blog-content'>
+                                <div button-create-blog-text>
+                                    Create
+                                </div>
+                            </div>)
+                        }
+                    </button>
                 </div>
 
             </div>
